fa6f1862f0eb4d471072bd7ea939fbb7fa4cb97d
4LastBreath <04lastbreath@gmail.com>
2024-08-12 14:15:49 +0200

    navlinks fixe

--- src/utils/navLinks.ts
+++ src/utils/navLinks.ts
@@ -2,16 +2,16 @@ export const navLinks = [
   {
     name: 'Accueil',
     path: '/portfolio/#hero',
-    isActive: (pathname: string, hash: string) => pathname === '/portfolio' && hash === '#hero',
+    isActive: (pathname: string, hash: string) => pathname === '/portfolio/' && hash === '#hero',
   },
   {
     name: 'Compétences',
     path: '/portfolio/#skills',
-    isActive: (pathname: string, hash: string) => pathname === '/portfolio' && hash === '#skills',
+    isActive: (pathname: string, hash: string) => pathname === '/portfolio/' && hash === '#skills',
   },
   {
     name: 'Projets',
     path: '/portfolio/#projects',
-    isActive: (pathname: string, hash: string) => pathname === '/portfolio' && hash === '#projects',
+    isActive: (pathname: string, hash: string) => pathname === '/portfolio/' && hash === '#projects',
   }
 ];
