563f512e2bb5968aefa3dccdd03e1335ebef4664
4LastBreath <04lastbreath@gmail.com>
2024-08-18 14:44:06 +0200

    test

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Navigate } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/home";
 import Contact from "../pages/contact";
 import NotFound from "../pages/notFound";
@@ -10,12 +10,8 @@ export const router = createBrowserRouter([
     element: <Home />, 
   },
   {
-    path: '/portfolio/404',
-    element: <NotFound />,
-  },
-  {
     path: '*',
-    element: <Navigate replace to='/portfolio/404'/>
+    element: <NotFound />,
   },
   {
     path: '/portfolio/contact',
