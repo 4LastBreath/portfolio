a9450fb86b02d9dae884723c48f39c00e0f2057f
4LastBreath <04lastbreath@gmail.com>
2024-08-21 12:24:33 +0200

    testing stuff

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,6 +1,6 @@
 import ContactForm from '../components/contact/contactForm';
 import Layout from '../layouts';
-import { Section, SectionParagraph, SectionTitle } from '../layouts/section';
+import { Section, SectionTitle } from '../layouts/section';
 
 const Contact = () => {
 
@@ -9,13 +9,13 @@ const Contact = () => {
   <Section id='contact' firstSection>
 
     <SectionTitle>Contact</SectionTitle>
-    <SectionParagraph>
-      Vous pouvez me contacter par mail, téléphone ou avec le formulaire ci-dessous:
+    <div className='section_p'>
+      <p>Vous pouvez me contacter par mail, téléphone ou avec le formulaire ci-dessous:</p>
       <ul className='contact_cord | mt-200'>
         <li>4brian.fritz@gmail.com</li>
         <li>06 48 64 89 77</li>
       </ul>
-    </SectionParagraph>
+    </div>
     <ContactForm />
 
   </Section>
